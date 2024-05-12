e5e8f138b17beb74667445a89cdda510adf9545d
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-12 10:28:51 -0300

    :bookmark: 0.2.0

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='missing_mga',
-    version='0.1',
+    version='0.2.0',
     packages=find_packages(),
     install_requires=['pandas', 'numpy', 'matplotlib', 'seaborn', 'itertools', 'upsetplot'],
     author='Mariano Gobea Alcoba',
     author_email='gobeamariano@gmail.com',
     description='A package for handling missing values in datasets.',
-    url='https://github.com/Mgobeaalcoba/missing_mga.git',
+    long_description=long_description,  # Usa el contenido del README.md como descripción larga
+    long_description_content_type="text/markdown",  # Especifica el tipo de contenido como markdown
+    url='https://github.com/Mgobeaalcoba/missing_mga',
     license='MIT',
 )
